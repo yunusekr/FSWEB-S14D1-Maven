4bcc1edac45958e08217ce66e4bef4ef777430c7
yunusekr <yunus_eker94@hotmail.com>
2024-07-29 15:49:42 +0300

    tasks completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import com.workintech.cylinder.Circle;
+import com.workintech.cylinder.Cylinder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Circle circle = new Circle(3.75);
+
+        System.out.println("circle.radius= " + circle.getRadius());
+
+        System.out.println("circle.area= " + circle.getArea());
+
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+
+        System.out.println("cylinder.radius= " + cylinder.getRadius());
+
+        System.out.println("cylinder.height= " + cylinder.getHeight());
+
+        System.out.println("cylinder.area= " + cylinder.getArea());
+
+        System.out.println("cylinder.volume= " + cylinder.getVolume());
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -13,7 +13,7 @@ import java.lang.reflect.Field;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(ResultAnalyzer.class)
 public class MainTest {
